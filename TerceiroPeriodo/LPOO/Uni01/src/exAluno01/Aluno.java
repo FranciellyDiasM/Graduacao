fcdbf6f93b5a95c5fe2c9a953c450a91625384b0
FranciellyDiasM <frandiasma@gmail.com>
2024-03-11 17:31:18 -0300

    criacao classe aluno

---
+++ TerceiroPeriodo/LPOO/Uni01/src/exAluno01/Aluno.java
@@ -0,0 +1,65 @@
+package exAluno01;
+
+public class Aluno {
+	
+	private String nome;
+	private int matricula;
+	private double nota1;
+	private double nota2;
+	
+	
+	
+	public Aluno(String nome) {
+		this.nome = nome;
+	}
+
+	public Aluno(String nome, int matricula, double nota1, double nota2) {
+		this.nome = nome;
+		this.matricula = matricula;
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+	}
+	
+	public Aluno() {
+		this.nome = "";
+		this.matricula = 0;
+		this.nota1 = 0;
+		this.nota2 = 0;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	
+	public boolean compareTo (Aluno outroAluno) {
+		return this.nome.equalsIgnoreCase(outroAluno.getNome());
+		
+	}
+
+	public String toString() {
+		return "Aluno [nome= " + nome + ", matricula=" + matricula + ", nota1=" + nota1 + ", nota2=" + nota2 + "]";
+	}
+	
+	
+}
--- TerceiroPeriodo/LPOO/Uni01/src/exAluno01/AlunoMain.java
+++ TerceiroPeriodo/LPOO/Uni01/src/exAluno01/AlunoMain.java
@@ -3,8 +3,7 @@ package exAluno01;
 public class AlunoMain {
 
 	public static void main(String[] args) {
-		System.out.println("Olá Mundo!");
-
+		
 	}
 
 }
